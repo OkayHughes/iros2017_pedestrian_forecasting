191d2cdece27ff98648e218845bd621147402af8
Owen Hughes <okayhughes@gmail.com>
2018-05-11 13:25:11 -0400

    Brings integrate.py into python3 compliance. Pylint score of 10.00/10.

--- code/integrate.py
+++ code/integrate.py
@@ -1,6 +1,11 @@
+"""Integration Module
+
+Contains core quadrature routine that is used throughout the other modules.
+"""
+from functools import reduce
 import numpy as np
 
-def trap_quad( integrand, bounds, res = None ):
+def trap_quad(integrand, bounds, res=None):
     """ Computes integrals using the n-dimensional trapezoid rule
     args:
         integrand: callable
@@ -16,52 +21,54 @@ def trap_quad( integrand, bounds, res = None ):
 
     if res is None:
         res = (len(bounds)/2)*(100,)
-    if type(res) == int:
+    if isinstance(res, int):
         res = (res,)
-    assert( len(res)*2 is len(bounds) )
+    assert len(res)*2 is len(bounds)
     def weights_func(r):
+        """
+        Routine to calculate quadrature weights for trapezoid rule.
+        """
         weights_1d = np.ones(r)
         weights_1d[0], weights_1d[-1] = 0.5, 0.5
         return weights_1d
     lower_bnds = [bounds[2*k] for k in range(len(res))]
     upper_bnds = [bounds[2*k+1] for k in range(len(res))]
-    grid = np.meshgrid(
-            *[np.linspace(lower_bnds[k], upper_bnds[k],res[k])
-            for k in range(len(res))])
-    dV = reduce( lambda x,y:x*y, map( lambda mn,mx,r: (mx-mn)/float(r-1), lower_bnds, upper_bnds, res ))
-    weights = dV*reduce( np.outer, map( weights_func, res) )
-    return np.einsum( 'i,i', weights.flatten(), integrand( *grid ).flatten() ) 
+    grid = np.meshgrid(*[np.linspace(lower_bnds[k], upper_bnds[k], res[k])
+                         for k in range(len(res))])
+    dV = reduce(lambda x, y: x * y,
+                map(lambda mn, mx, r: (mx - mn) / float(r - 1),
+                    lower_bnds, upper_bnds, res))
+    weights = dV*reduce(np.outer, map(weights_func, res))
+    return np.einsum('i,i', weights.flatten(), integrand(*grid).flatten())
 
 if __name__ == '__main__':
     integrand = lambda x: x**2
     res = 50
-    bounds = (0,1)
-    q = trap_quad( integrand, bounds, res )
-    print "Testing 1 dimensional quadrature"
-    print "result         = {q}".format(q=q)
-    print "correct answer = {a}".format(a=1.0/3.0)
-    print "error = {error}".format(error=q-1.0/3.0)
+    bounds = (0, 1)
+    q = trap_quad(integrand, bounds, res)
+    print("Testing 1 dimensional quadrature")
+    print("result         = {q}".format(q=q))
+    print("correct answer = {a}".format(a=1.0/3.0))
+    print("error = {error}".format(error=q-1.0/3.0))
 
-    print "\nTesting 2 dimensional quadrature."
-    print "Integrand = ( f(x,y)= exp(-x) )"
-    integrand = lambda x,y: np.exp( - x)
+    print("\nTesting 2 dimensional quadrature.")
+    print("Integrand = ( f(x,y)= exp(-x) )")
+    integrand = lambda x, y: np.exp(-x)
     bounds = (-1, 1, -1, 1)
-    print "Domain = [{}, {}] x [{}, {}]".format( *bounds)
+    print("Domain = [{}, {}] x [{}, {}]".format(*bounds))
     res = (100, 100)
-    q = trap_quad( integrand, bounds, res )
-    a = 2*( np.exp(1) - np.exp(-1) )
-    print "result         = {q}".format(q=q)
-    print "correct answer = {a}".format(a=a)
-    print "error = {error}".format(error=q-a)
+    q = trap_quad(integrand, bounds, res)
+    a = 2 * (np.exp(1) - np.exp(-1))
+    print("result         = {q}".format(q=q))
+    print("correct answer = {a}".format(a=a))
+    print("error = {error}".format(error=q-a))
 
-    print "\n Testing 3 dimensional quadrature."
-    integrand = lambda x,y,z: x+y*z
-    bounds = (0,1,0,1,0,1)
-    res = (100,100,101)
-    q = trap_quad( integrand, bounds, res)
+    print("\n Testing 3 dimensional quadrature.")
+    integrand = lambda x, y, z: x + y * z
+    bounds = (0, 1, 0, 1, 0, 1)
+    res = (100, 100, 101)
+    q = trap_quad(integrand, bounds, res)
     a = 0.75
-    print "result         = {q}".format(q=q)
-    print "correct answer = {a}".format(a=a)
-    print "error = {error}".format(error=q-a)
-
-
+    print("result         = {q}".format(q=q))
+    print("correct answer = {a}".format(a=a))
+    print("error = {error}".format(error=q-a))
