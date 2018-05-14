1d5c024be3d83cf9b349fbb81c7c03ef78fd7330
Owen Hughes <okayhughes@gmail.com>
2018-05-14 15:17:10 -0400

    Brings varidis.py into python3 compliance. pylint score of 10/10

--- code/varidis.py
+++ code/varidis.py
@@ -1,7 +1,11 @@
+"""Varidis module
+Contains the colormap `varidis`, which is viridis but with transparency proportional to the color,
+i.e. purple is transparent, yellow-green is fully opaque, with a linear interpolation in between.
+"""
 from matplotlib.colors import ListedColormap
 from matplotlib import cm
 import numpy as np
-_cmap = cm.viridis
+_cmap = cm.viridis # pylint: disable=no-member
 varidis = _cmap(np.arange(_cmap.N))
-varidis[:,-1] = np.linspace(0, 1, _cmap.N)
+varidis[:, -1] = np.linspace(0, 1, _cmap.N)
 varidis = ListedColormap(varidis)
