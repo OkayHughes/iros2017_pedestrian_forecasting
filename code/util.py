3d30ab4e1e1bfe28b288eb822a4a41829366c8d4
Owen Hughes <okayhughes@gmail.com>
2018-05-14 15:12:53 -0400

    Brings util.py into python3 compliance. pylint score of 10/10.

--- code/util.py
+++ code/util.py
@@ -1,6 +1,20 @@
+"""Miscellaneous Utilities Module
+Due to a dependency cycle, these were unable to be placed in helper routines,
+and contain utilities for dealing with the json config files.
+
+"""
 import os
 import json
 def read_json(fname):
+    """
+    Function reads json string from a file, sanitizes it,
+    and returns a python dictionary with the contents.
+
+    Takes:
+        fname: string, file path to json
+    Returns:
+        dic: dict, de-serialized json
+    """
     with open(fname) as f:
         st = f.read()
     json_acceptable_string = st.replace("'", "\"")
